be5dee3105395bb1d6d1811e46d7dc67deb8ed03
priyanka-bindal <bidnalpriyanka@gmail.com>
2025-10-23 16:13:04 +0530

    save to grade

--- grade.py
+++ grade.py
@@ -1,12 +1,21 @@
-marks = float(input("Enter your marks (out of 100): "))
-if marks >= 90:
-    grade = 'A'
-elif marks >= 75:
-    grade = 'B'
-elif marks >= 60:
-    grade = 'C'
-elif marks >= 50:
-    grade = 'D'
+marks = []
+for i in range(5):
+    mark = float(input(f"Enter marks for subject {i+1}: "))
+    marks.append(mark)
+
+average = sum(marks) / 5
+
+if average >= 90:
+    grade = "A"
+elif average >= 75:
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 40:
+    grade = "D"
 else:
-    grade = 'Fail'
-print(f"Your grade is: {grade}")
+    grade = "Fail"
+
+print(f"Average Marks: {average}")
+
+print(f"Grade: {grade}")
